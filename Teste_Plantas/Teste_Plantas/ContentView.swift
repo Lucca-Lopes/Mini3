e8a308a7fe507fe746b7e5ed074c6d4908b39fff
Lucca-Lopes <luccavlopes@hotmail.com>
2023-03-27 15:58:02 -0300

    Revert "Merge pull request #4 from Lucca-Lopes/Luccao"
    This reverts commit 3b14720dd3dbe9b74554781436ba9f0237ffeb9b, reversing
    changes made to d534829044bab0cad344c26d0553ee5b3c929282.

--- Teste_Plantas/Teste_Plantas/ContentView.swift
+++ Teste_Plantas/Teste_Plantas/ContentView.swift
@@ -8,40 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var scale = 0.8
-//    var dicionario: [Int : [Bool]]
     
     var body: some View {
-        VStack {
-            Image(systemName: "arrow.right")
-//                .imageScale(.large)
-//                .foregroundColor(.accentColor)
-                .resizable()
-                .scaledToFit()
-                .frame(width: 50)
-                .padding(20)
-                .background(Color.green)
-                .foregroundColor(Color.black)
-                .clipShape(Circle())
-//            criando a animacão de pulsar em cima do circulo
-                .overlay(
-                    Circle()
-                        .stroke(Color.gray)
-                        .scaleEffect(scale)
-                        .opacity(Double(2 - scale)) //conforme aumenta o scale diminui a opacidade
-                        .animation(
-                            Animation.easeOut(duration: 1)
-                                .repeatForever(autoreverses: false)
-                        
-                        )
-                )
-                
-        }.onAppear(
-//            self.scale = 1
-        )
-        
+        Text("Hello");
     }
-    
 }
 
 //struct ContentView_Previews: PreviewProvider {
