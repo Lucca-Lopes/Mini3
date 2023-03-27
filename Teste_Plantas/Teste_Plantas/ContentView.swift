fdb10726469fc03fb9dbef9e470b62e9e61863f4
Lucca-Lopes <luccavlopes@hotmail.com>
2023-03-27 15:56:33 -0300

    Revert "Merge branch 'main' into Luccao"
    This reverts commit 328cd5554cb52ee314630287c9e6fe00726ef80b, reversing
    changes made to 5e57087f1eb5c7a06eae490b2563d4319586381c.

--- Teste_Plantas/Teste_Plantas/ContentView.swift
+++ Teste_Plantas/Teste_Plantas/ContentView.swift
@@ -8,10 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var scale = 0.8
+//    var dicionario: [Int : [Bool]]
     
     var body: some View {
-        Text("Hello");
+        VStack {
+            Image(systemName: "arrow.right")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 50)
+                .padding(20)
+                .background(Color.green)
+                .foregroundColor(Color.black)
+                .clipShape(Circle())
+//            criando a animacão de pulsar em cima do circulo
+                .overlay(
+                    Circle()
+                        .stroke(Color.gray)
+                        .scaleEffect(scale)
+                        .opacity(Double(2 - scale)) //conforme aumenta o scale diminui a opacidade
+                        .animation(
+                            Animation.easeOut(duration: 1)
+                                .repeatForever(autoreverses: false)
+                        
+                        )
+                )
+                
+        }.onAppear(
+//            self.scale = 1
+        )
+        
     }
+    
 }
 
 //struct ContentView_Previews: PreviewProvider {
--- Teste_Plantas/Teste_Plantas/SplashScreen.swift
+++ Teste_Plantas/Teste_Plantas/SplashScreen.swift
@@ -8,34 +8,26 @@
 import SwiftUI
 
 struct SplashScreen: View {
-    @State private var showHomeView = false
     @State var scale = 0.6
     @State var opacity = 0.6
     
     var body: some View {
-        if showHomeView {
-            HomeView()
-        } else {
-            VStack(){
-                Image("girassol")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 250, height: 250)
-                Text("Naturee")
-                    .font(.system(size: 40, weight: .bold, design: .rounded))
-                    .foregroundColor(Color(red: 0.875, green: 0.651, blue: 0.482))
-            }
-            .scaleEffect(scale)
-            .opacity(opacity)
-            .onAppear{
-                withAnimation(
-                    .easeIn(duration: 2)){
-                        self.scale = 1.0
-                        self.opacity = 1.0
-                    }
-                withAnimation(Animation.linear.delay(2)) {
-                    showHomeView = true
-                }
+        VStack(){
+            Image("girassol")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 250, height: 250)
+            Text("Naturee")
+                .font(.system(size: 40, weight: .bold, design: .rounded))
+                .foregroundColor(Color(red: 0.875, green: 0.651, blue: 0.482))
+        }
+        .scaleEffect(scale)
+        .opacity(opacity)
+        .onAppear{
+            withAnimation(
+                .easeIn(duration: 2)){
+                self.scale = 1.0
+                self.opacity = 1.0
             }
         }
     }
