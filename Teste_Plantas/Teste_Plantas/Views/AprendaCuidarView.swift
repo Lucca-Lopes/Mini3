02f49af79afe7f51d68d4e6e89894ba457321a8d
Lucca-Lopes <luccavlopes@hotmail.com>
2023-03-28 14:48:44 -0300

    merge: branch config

--- Teste_Plantas/Teste_Plantas/Views/AprendaCuidarView.swift
+++ Teste_Plantas/Teste_Plantas/Views/AprendaCuidarView.swift
@@ -7,9 +7,54 @@
 
 import SwiftUI
 
-struct AprendaCuidarView: View {
+struct MiniGameView: View {
     var body: some View {
-        Text("Aprenda cuidar aqui")
+        VStack{
+            NavigationLink(destination:{
+                ContentView() //chamar tela certa
+            }, label: {
+                Text("Rega")
+                    .font(.system(size: 20, weight: .semibold))
+                    .foregroundColor(.brown)
+                    .frame(width: 200, height: 100)
+                    .background(
+                        Color.yellow
+                            .cornerRadius(10)
+                            .shadow(radius: 8)
+                        )
+                    .padding()
+            })
+            
+            NavigationLink(destination: {
+                ContentView() //chamar tela certa
+            }, label: {
+                Text("Sol")
+                    .font(.system(size: 20, weight: .semibold))
+                    .foregroundColor(.brown)
+                    .frame(width: 200, height: 100)
+                    .background(
+                        Color.yellow
+                            .cornerRadius(10)
+                            .shadow(radius: 8)
+                        )
+                    .padding()
+            })
+            
+            NavigationLink(destination: {
+                ContentView() //chamar tela certa
+           }, label: {
+               Text("Saúde")
+                   .font(.system(size: 20, weight: .semibold))
+                   .foregroundColor(.brown)
+                   .frame(width: 200, height: 100)
+                   .background(
+                       Color.yellow
+                           .cornerRadius(10)
+                           .shadow(radius: 8)
+                       )
+                   .padding()
+           })
+        }
     }
 }
 
--- Teste_Plantas/Teste_Plantas/Views/ConfigView.swift
+++ Teste_Plantas/Teste_Plantas/Views/ConfigView.swift
@@ -9,7 +9,52 @@ import SwiftUI
 
 struct ConfigView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            VStack{
+                NavigationLink(destination: {
+                    SobreView()
+                }, label: {
+                    Text("Sobre nós")
+                        .font(.system(size: 20, weight: .semibold))
+                        .foregroundColor(.brown)
+                        .frame(width: 200, height: 100)
+                        .background(
+                            Color.yellow
+                                .cornerRadius(10)
+                                .shadow(radius: 8)
+                            )
+                        .padding()
+                })
+                
+                NavigationLink(destination: {
+                    PrivacidadeView()
+                }, label: {
+                    Text("Política de Privacidade")
+                        .font(.system(size: 20, weight: .semibold))
+                        .foregroundColor(.brown)
+                        .frame(width: 200, height: 100)
+                        .background(
+                            Color.yellow
+                                .cornerRadius(10)
+                                .shadow(radius: 8)
+                            )
+                        .padding()
+                })
+                
+                NavigationLink(destination: {
+                    DireitosView()
+                }, label: {
+                    Text("Direitos Autorais")
+                        .font(.system(size: 20, weight: .semibold))
+                        .foregroundColor(.brown)
+                        .frame(width: 200, height: 100)
+                        .background(
+                            Color.yellow
+                                .cornerRadius(10)
+                                .shadow(radius: 8)
+                            )
+                        .padding()
+                })
+            }
     }
 }
 
